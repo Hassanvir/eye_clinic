4504c01051164d26af0d40359e3ef9319d39f1d2
Hassanvir Singh <hassanvirsingh5@gmail.com>
2026-03-18 11:49:34 -0600

    updated maps

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -41,6 +41,7 @@ const Contact = () => {
   return (
     <section id="contact" className="flex-center section-padding">
       <div className="w-full h-full md:px-10 px-5">
+
         <TitleHeader
           title="Get in Touch – Let’s Connect"
           sub="💬 Have questions or ideas? Let’s talk! 🚀"
